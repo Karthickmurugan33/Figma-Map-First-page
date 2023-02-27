3610e051cc3f9a9faedfed734256abea745cd624
karthickmurugan33 <karrthick33@gmail.com>
2023-02-27 11:46:37 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Nav from "../src/components/Nav"
+import UserDetail from './components/UserDetail';
+import Footer from './components/Footer';
+import Map from './components/Map';
 
 function App() {
   return (
     <div className="App">
-
+      <Nav />
+      <UserDetail />
+      <Map />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,73 @@
+import React from "react";
+import style from "../styles/footer.module.css";
+const Footer = () => {
+  return (
+    <div>
+      <div className={style.value}>
+        <div className={style.salesfigure}>
+          <p className={style.title}> Sales Figures</p>
+          <h5 className={style.amount}>$10,430</h5>
+        </div>
+        <span className={style.barmain}>
+          <span className={style.bar1}></span>
+          <span className={style.bar2}></span>
+          <span className={style.bar3}></span>
+          <span className={style.bar4}></span>
+          <span className={style.bar5}></span>
+          <span className={style.bar6}></span>
+          <span className={style.bar7}></span>
+          <span className={style.bar8}></span>
+          <span className={style.bar9}></span>
+          <span className={style.bar10}></span>
+          <span className={style.bar11}></span>
+          <span className={style.bar12}></span>
+          <span className={style.bar13}></span>
+          <span className={style.bar14}></span>
+          <span className={style.bar15}></span>
+          <span className={style.bar16}></span>
+          <span className={style.bar17}></span>
+          <span className={style.bar18}></span>
+          <span className={style.bar19}></span>
+          <span className={style.bar20}></span>
+          <span className={style.bar21}></span>
+          <span className={style.bar22}></span>
+          <span className={style.bar23}></span>
+          <span className={style.bar24}></span>
+          <span className={style.bar25}></span>
+          <span className={style.bar26}></span>
+          <span className={style.bar27}></span>
+          <span className={style.bar28}></span>
+          <span className={style.bar29}></span>
+          <span className={style.bar30}></span>
+          <span className={style.bar31}></span>
+          <span className={style.bar32}></span>
+          <span className={style.bar33}></span>
+          <span className={style.bar34}></span>
+          <span className={style.bar35}></span>
+          <span className={style.bar36}></span>
+          <span className={style.bar37}></span>
+          <span className={style.bar38}></span>
+          <span className={style.bar39}></span>
+          <span className={style.bar40}></span>
+          <span className={style.bar41}></span>
+          <span className={style.bar42}></span>
+          <span className={style.bar43}></span>
+          <span className={style.bar44}></span>
+          <span className={style.bar45}></span>
+          <span className={style.bar46}></span>
+          <span className={style.bar47}></span>
+          <span className={style.bar48}></span>
+          <span className={style.bar49}></span>
+          <span className={style.bar50}></span>
+        </span>
+        <span className={style.line1}></span>
+        <span className={style.line2}></span>
+        <span className={style.line3}></span>
+      </div>
+
+
+    </div>
+  );
+};
+
+export default Footer;
